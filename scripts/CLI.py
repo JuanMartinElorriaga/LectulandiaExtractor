38e38a6d5b9a6c10c933b85344f797ca3643c9c4
JuanMartinElorriaga <juanmartinelorriaga123@gmail.com>
2024-02-12 20:44:48 -0300

    add optional book selection

--- scripts/CLI.py
+++ scripts/CLI.py
@@ -15,12 +15,26 @@ def main(author, proxy, output_dir):
         urls_from_author = downloader.get_urls_from_author_url(author_url)
         click.echo(f'Total URLs found for {author}: {len(urls_from_author)}')
 
-        download_links = downloader.get_batch_download_links(urls_from_author)
+        # Present the list of URLs to the user for selection
+        selected_urls = click.prompt(
+            'Select URLs to download (comma-separated, e.g., 1,2,3)',
+            type         = str,
+            default      = '1', # Default value can be adjusted
+            show_default = True
+        ).split(',')
+
+        # Filter the URLs based on user selection
+        selected_urls = [urls_from_author[int(index) - 1] for index in selected_urls]
+
+        click.echo(f'Selected URLs: {selected_urls}')
+
+        download_links = downloader.get_batch_download_links(selected_urls)
         click.echo(f'Total download links found for {author}: {len(download_links)}')
 
         if not click.confirm('Do you want to start downloading the books?', default=True):
             raise click.Abort("Download aborted by user.")
 
+        downloader.batch_download_books(download_links, author)
         click.echo('Download completed successfully!')
 
     except Exception as e:
