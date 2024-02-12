2b27f1c01ccc901d541537c2c237b2d408860555
JuanMartinElorriaga <juanmartinelorriaga123@gmail.com>
2024-02-11 22:01:37 -0300

    update extractor

--- scripts/extractor.py
+++ scripts/extractor.py
@@ -1,21 +1,31 @@
 import logging
-logging.basicConfig(level=logging.INFO)
 import os
 import re
 import time
-from queue import Queue
-from threading import Thread
 import werkzeug
 werkzeug.cached_property = werkzeug.utils.cached_property
 from robobrowser import RoboBrowser
-from requests import Session
-from requests.exceptions import Timeout
+import requests
 from unidecode import unidecode
+
+# Set logging to log into an external file as well as stream in the console
+import logging
+logging.basicConfig(filename='Downloader_log.txt',
+                    level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+# Create a StreamHandler to print log messages to the console
+console_handler   = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(console_formatter)
+
+# Add the StreamHandler to the logger
+logging.getLogger().addHandler(console_handler)
 
 class Downloader():
     def __init__(self, proxy=None, worker_num=0):
         self.worker_num = worker_num
-        session         = Session()
+        session         = requests.Session()
         session.headers.update({
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
             })
@@ -25,13 +35,20 @@ class Downloader():
 
 
     def get_author_url(self, author):
-        ''' Get author url from input given '''
+        ''' Get author url from author name given as input '''
+        # Check if base url is active
+        base_url = 'https://ww3.lectulandia.com/autor/'
+        response = requests.head(base_url)
+        if response.status_code != 200:
+            raise requests.ConnectionError("Base URL is not active. Please set a different base URL")
+        # Create author url
         author_name = author.replace(" ", "-").lower()
         author_url  = f'https://ww3.lectulandia.com/autor/{author_name}'
         return author_url
 
 
     def get_books_titles_from_author_url(self, author_url):
+        ''' Get book titles from a given author url '''
         self.browser.open(author_url)
         books_titles_from_author = [
             f"{book['title']}"
@@ -41,26 +58,24 @@ class Downloader():
 
 
     def get_urls_from_author_url(self, author_url, urls_from_author=None):
-        # TODO: recopilar TODAS las paginas, no solo la primera
+        ''' Get list of book links from a given author'''
         if urls_from_author is None:
             urls_from_author = []
-
         self.browser.open(author_url)
         new_urls = [
             f"https://ww3.lectulandia.com{book['href']}"
             for book in self.browser.find_all("a", class_="card-click-target")]
         urls_from_author.extend(new_urls)
-
         next_page_link = self.browser.find("a", class_="next page-numbers")
         if next_page_link:
             next_page_url = f"https://ww3.lectulandia.com{next_page_link['href']}"
+            # Recursive call as long as there is a "Siguiente" button
             self.get_urls_from_author_url(next_page_url, urls_from_author)
-            #next_page_urls = self.get_urls_from_author_url(next_page_url)  # Recursive call
-
         return urls_from_author
 
 
     def get_download_link(self, book_url):
+        ''' Turn book link into a download url '''
         self.browser.open(book_url)
         for link in self.browser.find_all("a"):
             if "download.php?t=1" in str(link):
@@ -68,25 +83,24 @@ class Downloader():
 
 
     def get_batch_download_links(self, urls_from_author):
+        ''' Get whole list of links to download from a given author '''
         download_links = [self.get_download_link(book_url) for book_url in urls_from_author]
         return download_links
 
 
     def download_book(self, download_url, author, timeout=180):
-        # Replace with library folder
-        library_folder = "F:\Calibre Library"
+        ''' Download a book and save into local directory if not exists already '''
+        library_folder = "F:\Calibre Library" # Modify if necessary
         author_folder  = os.path.join(library_folder, author)
         try:
             if not os.path.exists(author_folder):
                 os.makedirs(author_folder)
-
             logging.info(f'Downloading book from {download_url}')
             self.browser.open(download_url)
             pattern = re.compile("var linkCode = \"(.*?)\";")
             section = pattern.findall(str(self.browser.parsed))
             ant_url = f'https://www.antupload.com/file/{section[0]}'
             self.browser.open(ant_url)
-
             filename = self.browser.find(
                 "div", id="fileDescription").find_all("p")[1].text.replace(
                     "Name: ", "")
@@ -95,36 +109,33 @@ class Downloader():
                 "div", id="fileDescription").find_all("p")[2].text
             file_url = self.browser.find("a", id="downloadB")
             logging.info(size)
-            time.sleep(2)
-
             # Check if the file already exists in the target directory
             file_path = os.path.join(author_folder, filename)
             if os.path.exists(file_path):
                 logging.info(f'File already exists: {file_path}')
                 return None
-
             self.browser.follow_link(file_url, timeout=timeout)
-
             with open(file_path, "wb") as epub_file:
                 epub_file.write(self.browser.response.content)
                 logging.info(f'File has been saved to: {epub_file.name}')
                 return filename, size
 
-        except Timeout:
+        except requests.exceptions.Timeout:
             logging.error(f'Timeout error during download. URL: {download_url}')
             return None
-
         except Exception as e:
             logging.error(f'Error downloading book: {str(e)}')
             return None
 
     def batch_download_books(self, urls_from_author, author):
+        ''' Download all book collection from a given author'''
         for url in urls_from_author:
             self.download_book(url, author)
-            #time.sleep(1)
+            time.sleep(1)
 
 
     def get_book_page_list(self, page:int):
+        ''' Get list of book titles from a given page number '''
         self.browser.open(f'https://ww3.lectulandia.com/book/page/{page}/')
         return [
             f"https://ww3.lectulandia.com{book['href']}"
@@ -133,6 +144,7 @@ class Downloader():
 
 
     def download_full_page(self, page:int):
+        ''' Download a full page '''
         logging.info(f"Downloading page: {page} ")
         try:
             books = self.get_book_page_list(page)
@@ -142,47 +154,3 @@ class Downloader():
                 logging.info(f"Worker: {self.worker_num} on page: {page}", self.download_book(download_url))
         except Exception as e:
             logging.error(f'Error downloading full page: {str(e)}')
-
-
-class Worker(Thread):
-    def __init__(self, queue, worker_number, proxy=None):
-        Thread.__init__(self)
-        self.queue      = queue
-        self.downloader = Downloader(proxy)
-        self.worker_num = worker_number
-
-    def run(self):
-        while True:
-            page = self.queue.get()
-            try:
-                logging.info(f"Worker: {self.worker_num} downloading page: {page}")
-                self.downloader.download_full_page(page)
-            except Exception as e:
-                logging.error(f'Error in worker {self.worker_num} while processing page {page}: {str(e)}')
-            finally:
-                self.queue.task_done()
-
-
-def main():
-    pages   = [x + 1 for x in range(8)]
-    proxies = None #[None, "https://188.168.75.254:56899"]
-    queue   = Queue()
-
-    # Creating workers
-    workers = []
-    for x in range(2):
-        worker = Worker(queue, x, proxies[x])
-        worker.daemon = True
-        worker.start()
-        workers.append(worker)
-
-    # Enqueueing pages
-    for page in pages:
-        queue.put(page)
-
-    # Waiting for all tasks to finish
-    queue.join()
-
-    # Waiting for all workers to finish
-    for worker in workers:
-        worker.join()
