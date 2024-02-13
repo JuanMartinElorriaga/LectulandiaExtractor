0730ed541e99f7ac1ecb8019532b3127fdd4676b
JuanMartinElorriaga <juanmartinelorriaga123@gmail.com>
2024-02-12 21:30:04 -0300

    add parsed list of books to select

--- scripts/CLI.py
+++ scripts/CLI.py
@@ -1,41 +1,50 @@
 import click
+from urllib.parse import urlparse, unquote
+
 from extractor import Downloader
 
 @click.command()
-@click.option('--author', prompt='Enter the author name', help='Author name to download books from')
-@click.option('--proxy', default=None, help='Proxy to be used for the requests')
-@click.option('--output-dir', default=r'F:\Calibre Library', help='Directory to save downloaded books')
+@click.option('--author', prompt='Nombre de autor', help='Nombre de autor desde el cual descargar libros')
+@click.option('--proxy', default=None, help='Proxy para los requests')
+@click.option('--output-dir', default=r'F:\Calibre Library', help='Directorio local en donde descargar los libros')
 def main(author, proxy, output_dir):
     downloader = Downloader(proxy=proxy, library_folder=output_dir)
 
     try:
         author_url = downloader.get_author_url(author)
-        click.echo(f'Author URL: {author_url}')
+        click.echo(f'URL del autor: {author_url}')
 
         urls_from_author = downloader.get_urls_from_author_url(author_url)
-        click.echo(f'Total URLs found for {author}: {len(urls_from_author)}')
+        click.echo(f'Total de links encontrados para {author.title()}: {len(urls_from_author)}')
 
-        # Present the list of URLs to the user for selection
-        selected_urls = click.prompt(
-            'Select URLs to download (comma-separated, e.g., 1,2,3)',
+        # Display the list of URLs to the user
+        click.echo('\nLista de libros:')
+
+        for i, url in enumerate(urls_from_author, start=1):
+            book = unquote(urlparse(url).path.strip('/').split('/')[-1]).replace('-', ' ')
+            click.echo(f"{i}. {book}")
+
+        # Prompt the user to select URLs for download
+        selected_indices = click.prompt(
+            'Seleccionar libro(s) para descargar (ej., 1,2,3)',
             type         = str,
-            default      = '1', # Default value can be adjusted
+            default      = 'ALL',
             show_default = True
         ).split(',')
 
         # Filter the URLs based on user selection
-        selected_urls = [urls_from_author[int(index) - 1] for index in selected_urls]
+        selected_urls = [urls_from_author[int(index) - 1] for index in selected_indices]
 
-        click.echo(f'Selected URLs: {selected_urls}')
+        #click.echo(f'Libros seleccionados: {selected_urls}')
 
         download_links = downloader.get_batch_download_links(selected_urls)
-        click.echo(f'Total download links found for {author}: {len(download_links)}')
+        click.echo(f'Total de libros encontrados para {author}: {len(download_links)}')
 
-        if not click.confirm('Do you want to start downloading the books?', default=True):
-            raise click.Abort("Download aborted by user.")
+        if not click.confirm('Quieres comenzar con la descarga?', default=True):
+            raise click.Abort("Descarga abortada por el usuario.")
 
         downloader.batch_download_books(download_links, author)
-        click.echo('Download completed successfully!')
+        click.echo('Descarga finalizada!')
 
     except Exception as e:
         click.echo(f'Error: {str(e)}')
